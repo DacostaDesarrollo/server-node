4e268ccae5dd7c451dcb260f97418f9a3aed45d3
Dacosta <dacostadesarrollo@gmail.com>
2022-07-23 11:59:15 -0500

    config docker y typeorm

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,5 @@
 import * as dotenv from "dotenv";
+import { ConnectionOptions } from "typeorm";
 
 export abstract class ConfigServer {
   constructor() {
@@ -28,5 +29,21 @@ export abstract class ConfigServer {
       arrEnv.unshift(...stringToArray);
     }
     return "." + arrEnv.join(".");
+  }
+  /**
+   * typeORMConfig
+   */
+  public get typeORMConfig():ConnectionOptions {
+    return {
+      type:     "mysql",
+      host:     this.getEnvironment("DB_HOST"),
+      port:     this.getNumberEnv("DB_PORT"),
+      username: this.getEnvironment("DB_USER"),
+      password: this.getEnvironment("DB_PASSWORD"),
+      database: this.getEnvironment("DB_DATABASE"),
+      entities: [__dirname+"../**/*.entity{.ts,.js}"],
+      migrations: [__dirname+"../../*migration/*{.ts,.js}"],
+      synchronize:true,
+    };
   }
 }   
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import cors from "cors";
 import { UserRouter } from "./router/user.router";
 import { ConfigServer } from "./config/config";
+import { Connection, createConnection } from "typeorm";
 
 class ServerBootstrap extends ConfigServer {
     public app: express.Application = express();
@@ -10,11 +11,12 @@ class ServerBootstrap extends ConfigServer {
 
     constructor() {
         super();
-        console.log(this.port);
-        
 
         this.app.use(express.json());
         this.app.use(express.urlencoded({extended:true}));
+
+        this.dbConnect();
+
         this.app.use(morgan('dev'));
         this.app.use(cors());
         this.app.use('/api',this.routers())
@@ -27,6 +29,10 @@ class ServerBootstrap extends ConfigServer {
      */
     public routers():Array<express.Router> {
         return [new UserRouter().router];
+    }
+
+    async dbConnect(): Promise<Connection> {
+        return await createConnection(this.typeORMConfig)
     }
 
     /**
